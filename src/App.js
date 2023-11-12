eed4a2734d3c9f469d96da70cbcadea75b4b3bbe
ryan <r.ong40915@gmail.com>
2023-11-12 02:28:06 -0500

    init commit from a template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navbar/NavBar';
-import Home from './components/home/Home';
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-
-import { Route, Routes } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <NavBar />
-      <div class="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </>
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
