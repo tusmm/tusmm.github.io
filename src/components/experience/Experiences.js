c7a74f5b6a36de07ccaa50176c895109f690430d
ryan <r.ong40915@gmail.com>
2023-11-14 00:16:00 -0500

    added hrefs to projects and experiences and completed descriptions

--- src/components/experience/Experiences.js
+++ src/components/experience/Experiences.js
@@ -40,28 +40,28 @@ export const Skills = () => {
       'alt': 'Image of Vanguard',
       'title': "Cloud Engineer Co-op · Vanguard",
       'text1': "I had the opportunity to work on a platform team as a Cloud Engineer in Vanguard. I worked with multiple AWS Services, deployed feature requests, and am currently developing a Python library for internal usage.",
-      'url': ""
+      'url': "https://investor.vanguard.com/corporate-portal"
     },
     {
       'src': meter1,
       'alt': 'Card image',
       'title': "Software Engineer Intern · M&T Bank",
       'text1': "At M&T Bank, another intern and I worked on building an internal Full Stack application from scratch using Spring Boot, Angular, and SQL. I worked mostly on the authentication and back end services.",
-      'url': ""
+      'url': "https://www3.mtb.com/careers/technology-internship-program"
     },
     {
       'src': vanguard,
       'alt': 'Card image',
       'title': "CS Homework Grader · RIT",
       'text1': "I prepared weekly Python scripts and grading scales to automate the grading of 47 students’ homework assignments while boosting my ability to give effective and detailed feedback and ability to explain technical concepts.",
-      'url': ""
+      'url': "https://www.rit.edu/computing/department-computer-science"
     },
     {
       'src': vanguard,
       'alt': 'Card image',
       'title': "Computational Physicist Research Intern · Tufts University",
       'text1': "Worked within a team of 5 developers and 8 polymer researchers to simulate the wetting phenomena through a replicated contact angle experiment by developing a shape optimization program.",
-      'url': ""
+      'url': "https://sites.tufts.edu/softmattertheory/"
     }
   ]
 
@@ -77,9 +77,10 @@ export const Skills = () => {
               <Row xs={1} md={2} className="g-4">
                 {experienceData.map((experience, idx) => (
                   <Col key={idx}>
+                    <a href={experience.url} className="card-link">
                     <Card bg='dark'
                       text='dark'
-                      style={{}}
+                      style={{cursor: "pointer"}}
                       className="bg-dark text-white">
                       <Ratio aspectRatio={9 / 16}>
                         <Card.Img variant="top"
@@ -93,6 +94,7 @@ export const Skills = () => {
                         </Card.Text>
                       </Card.Body>
                     </Card>
+                    </a>
                   </Col>
                 ))}
               </Row>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,6 +6,7 @@ import projImg1 from "../../assets/img/project-img1.png";
 import projImg2 from "../../assets/img/project-img2.png";
 import projImg3 from "../../assets/img/project-img3.png";
 import colorSharp2 from "../../assets/img/color-sharp2.png";
+import symptomtracker from "../../assets/img/symptom-tracker.jpg"
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -16,31 +17,37 @@ export const Projects = () => {
       title: "Qwerkey",
       description: "I worked in a group of five to develop a full stack application for a semester long project. Utilizing Git, Spring Boot, Angular, and an Agile methodology, we were created a Keyboard E-Commerce store that ultimately won first amongst 40 other students.",
       imgUrl: projImg1,
+      projUrl: ""
     },
     {
       title: "Hits2Fits",
-      description: "Design & Development",
+      description: "Used Spotify API, Google Cloud, Flask, Git, and a machine learning model to develop an outfit recommender based on an inputted song. I developed this in a team of 4 for a university hackation. I worked mainly on the backend logic and connectivity.",
       imgUrl: projImg2,
+      projUrl: "https://devpost.com/software/hits-to-fits"
     },
     {
       title: "Love @ First Bite",
-      description: "Design & Development",
+      description: "In a team of 4, my team and I developed a mobile app using React Native and Firebase for a hackathon which won Best UI/UX! Our goal was to match local, minority restaurants to users in a dating-app like fashion. I worked mostly on the backend and some front end screen components.",
       imgUrl: projImg3,
+      projUrl: "https://devpost.com/software/love-first-bite"
     },
     {
-      title: "Wall Scraper",
-      description: "Design & Development",
+      title: "Wallscraper",
+      description: "I developed this Python script to scrape wallpapers off a site. I used requests and Beautiful Soup Python libraries for this project. I built a user friendly command line interface with a search command for custom searches.",
       imgUrl: projImg1,
+      projUrl: "https://github.com/tusmm/wallscraper"
     },
     {
       title: "Hoppers (Academic)",
-      description: "Design & Development",
+      description: "For an academic project, I recreated the puzzle board game, 'Hoppers', using JavaFX and Java following a MVC structure. This application had a restart button, undo button, as well as a hint button. I implemented a BFS solver to make hints possible.",
       imgUrl: projImg2,
+      projUrl: "https://www.thinkfun.com/products/hoppers/"
     },
     {
       title: "Symptom Tracker",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      description: "Developed a Java application with a partner for individuals to enter and track their symptoms while graphing the severity of the symptoms to give users a visual of their illness. This project won 2nd place in the 'Health' category.",
+      imgUrl: symptomtracker,
+      projUrl: "https://devpost.com/software/illness-tracker"
     },
   ];
 
