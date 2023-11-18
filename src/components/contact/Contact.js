8bfe164386645d8b178cc707dec53df1f9763750
ryan <r.ong40915@gmail.com>
2023-11-18 15:42:58 -0500

    fixed contact form, forgot name attribute

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,24 +7,6 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
   return (
     <section className="contact" id="connect">
       <Container>
@@ -45,23 +27,18 @@ export const Contact = () => {
                   <input type="hidden" name="_subject" value="New submission!"/>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} required/>
+                      <input type="text" placeholder="Name" name="name" required/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} required/>
+                      <input type="email" placeholder="Email Address" name="email" required/>
                     </Col>
                     <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} required></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
+                      <textarea rows="6" type="text" placeholder="Message" name="message" required></textarea>
+                      <button type="submit">Send</button>
                     </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                      </Col>
-                    }
                   </Row>
                 </form>
+
                 <a class="-m-2 flex rounded-md p-2 text-neutral-300 hover:text-teal-500 focus:outline-none focus:ring-2 focus:ring-teal-500 hover:text-white" href="mailto:rto9185@rit.edu" target="_blank">
                   <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-4 w-4 flex-shrink-0 text-neutral-100 sm:h-5 sm:w-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
