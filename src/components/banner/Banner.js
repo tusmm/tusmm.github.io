c2c08292f56c6a5b120425b72a38b0d1dca30642
ryan <r.ong40915@gmail.com>
2023-11-19 01:02:09 -0500

    resume download fixes

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../../assets/img/laptop.png";
-import resume from "../../assets/Ryan-Ong-Resume.pdf";
+import resume from "../../assets/Ryan_Ong_Resume.pdf";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -66,7 +66,7 @@ export const Banner = () => {
                   <a href="#connect">
                     <button>Let’s Connect</button>
                   </a>
-                  <a href={resume} download>Resume
+                  <a href={resume} download="Ryan Ong - Resume.pdf">Resume
                     <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-1 w-1 text-white sm:h-1 sm:w-1"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
                   </a>
                 </div>}
