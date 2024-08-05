b84ef04b918cdb5ffd6272f3bc8ee4705ee6af3a
tusmm <r.ong40915@gmail.com>
2024-08-04 22:16:01 -0400

    new files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/navbar/NavBar";
-import { Banner } from "./components/banner/Banner";
-import { Skills } from "./components/experience/Experiences";
-import { Projects } from "./components/projects/Projects";
-import { Contact } from "./components/contact/Contact";
-import { Footer } from "./components/footer/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
     </div>
   );
 }
