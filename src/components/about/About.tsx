a1f2dcb7aa99f1dcf08858eb2b335061258f276f
Ryan Ong (rto9185) <r.ong40915@gmail.com>
2025-10-13 12:22:52 -0400

    Fix spelling error in About component description
    Corrected the spelling of 'impactful' in the About component.

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     return (
         <div className="about">
             <p className="about-description">
-                I was a student at Rochester Institute of Technology with a BS in Computer Science. And I absolutely love to build, be it projects, contraptions, or even Legos. I am passionate about learning more and developing robust and inpactful solutions to problems. 
+                I was a student at Rochester Institute of Technology with a BS in Computer Science. And I absolutely love to build, be it projects, contraptions, or even Legos. I am passionate about learning more and developing robust and impactful solutions to problems. 
                 <br />
                 <br />
                 I've had opportunities to work on various projects from full stack development to machine learning, and I am always looking to learn more. 
